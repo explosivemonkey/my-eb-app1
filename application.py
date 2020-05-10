b1b62601ce798087448359028974bbcef5acbb59
explosivemonkey <nick@explosivemonkey.com>
2020-05-10 20:15:00 +1000

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,8 @@
+import logging
 import logging.handlers
+
+from wsgiref.simple_server import make_server
+
 
 # Create logger
 logger = logging.getLogger(__name__)
@@ -126,26 +130,30 @@ welcome = """
 </html>
 """
 
-def app(environ, start_response):
-    path = environ['PATH_INFO']
-    method = environ['REQUEST_METHOD']
+def application(environ, start_response):
+    path    = environ['PATH_INFO']
+    method  = environ['REQUEST_METHOD']
     if method == 'POST':
         try:
             if path == '/':
                 request_body_size = int(environ['CONTENT_LENGTH'])
-                request_body = environ['wsgi.input'].read(request_body_size)
+                request_body = environ['wsgi.input'].read(request_body_size).decode()
                 logger.info("Received message: %s" % request_body)
             elif path == '/scheduled':
-                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'],
-                            environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
         except (TypeError, ValueError):
             logger.warning('Error retrieving request body for async work.')
         response = ''
     else:
         response = welcome
-    start_response("200 OK", [
-        ("Content-Type", "text/html"),
-        ("Content-Length", str(len(response)))
-    ])
-    return [bytes(response, 'utf-8')]
+    status = '200 OK'
+    headers = [('Content-type', 'text/html')]
 
+    start_response(status, headers)
+    return [response]
+
+
+if __name__ == '__main__':
+    httpd = make_server('', 8000, application)
+    print("Serving on port 8000...")
+    httpd.serve_forever()
